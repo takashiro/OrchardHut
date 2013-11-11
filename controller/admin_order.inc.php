34ad93a870ae5658cf02af03641c10400bf4916a
Takashiro <takashiro@qq.com>
2013-11-11 21:14:48 +0800

    Hide orders by their addresses
    Signed-off-by: Takashiro <takashiro@qq.com>

--- controller/admin_order.inc.php
+++ controller/admin_order.inc.php
@@ -2,44 +2,66 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-$db->select_table('order');
-$orders = $db->MFETCH('*', 'userid='.$_G['user']->id.' AND status<2 ORDER BY dateline DESC');
-
-$orderids = array();
-foreach($orders as &$o){
-	$orderids[] = $o['id'];
+$display_status = array();
+if($_G['admin']->hasPermission('order_sort')){
+	$display_status[] = 0;
 }
-unset($o);
-$orderids = implode(',', $orderids);
 
-$details = $db->fetch_all("SELECT p.name,d.subtype,d.amount,d.amountunit,d.number,d.orderid
-	FROM {$tpre}orderdetail d
-		LEFT JOIN {$tpre}product p ON p.id=d.productid
-	WHERE d.orderid IN ($orderids)");
-
-$order_details = array();
-foreach($details as &$d){
-	$order_details[$d['orderid']][] = $d['name'].(!empty($d['subtype']) ? '('.$d['subtype'].')' : '').' '.($d['amount'] * $d['number']).$d['amountunit'];
+if($_G['admin']->hasPermission('order_deliver')){
+	$display_status[] = 1;
 }
-unset($d);
 
-$addresses = $db->fetch_all("SELECT o.*,c.name componentname
-	FROM {$tpre}orderaddresscomponent o
-		LEFT JOIN {$tpre}addresscomponent c ON c.id=o.componentid
-	WHERE o.orderid IN ($orderids)");
+$display_status = implode(',', $display_status);
 
-$order_addresses = array();
-foreach($addresses as &$a){
-	$order_addresses[$a['orderid']][$a['formatid']] = $a['componentname'];
+if($_G['admin']->formatid > 0){
+	$formatid = $_G['admin']->formatid;
+	$componentid = $_G['admin']->componentid;
+	$orders = $db->fetch_all("SELECT o.* FROM {$tpre}orderaddresscomponent c
+		LEFT JOIN {$tpre}order o ON o.id=c.orderid
+		WHERE c.formatid=$formatid AND c.componentid=$componentid AND o.status IN ($display_status)");
+}else{
+	$db->select_table('order');
+	$orders = $db->MFETCH('*', "status IN ($display_status)");
 }
-unset($a);
 
-foreach($orders as &$o){
-	$o['items'] = implode('<br />', $order_details[$o['id']]);
-	$o['priceunit'] = Product::PriceUnits($o['priceunit']);
-	$o['address'] = &$order_addresses[$o['id']];
+if($orders){
+	$orderids = array();
+	foreach($orders as &$o){
+		$orderids[] = $o['id'];
+	}
+	unset($o);
+
+	$orderids = implode(',', $orderids);
+
+	$details = $db->fetch_all("SELECT p.name,d.subtype,d.amount,d.amountunit,d.number,d.orderid
+		FROM {$tpre}orderdetail d
+			LEFT JOIN {$tpre}product p ON p.id=d.productid
+		WHERE d.orderid IN ($orderids)");
+
+	$order_details = array();
+	foreach($details as &$d){
+		$order_details[$d['orderid']][] = $d['name'].(!empty($d['subtype']) ? '('.$d['subtype'].')' : '').' '.($d['amount'] * $d['number']).$d['amountunit'];
+	}
+	unset($d);
+
+	$addresses = $db->fetch_all("SELECT o.*,c.name componentname
+		FROM {$tpre}orderaddresscomponent o
+			LEFT JOIN {$tpre}addresscomponent c ON c.id=o.componentid
+		WHERE o.orderid IN ($orderids)");
+
+	$order_addresses = array();
+	foreach($addresses as &$a){
+		$order_addresses[$a['orderid']][$a['formatid']] = $a['componentname'];
+	}
+	unset($a);
+
+	foreach($orders as &$o){
+		$o['items'] = implode('<br />', $order_details[$o['id']]);
+		$o['priceunit'] = Product::PriceUnits($o['priceunit']);
+		$o['address'] = &$order_addresses[$o['id']];
+	}
+	unset($o);
 }
-unset($o);
 
 include view('order');
 
