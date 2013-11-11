6b3ee5a0eedc1d7d4815f585ad1019c04cb2deb9
Takashiro <takashiro@qq.com>
2013-11-11 20:56:57 +0800

    add a new page to manage orders
    Signed-off-by: Takashiro <takashiro@qq.com>

--- controller/admin_order.inc.php
+++ controller/admin_order.inc.php
@@ -2,6 +2,45 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
+$db->select_table('order');
+$orders = $db->MFETCH('*', 'userid='.$_G['user']->id.' AND status<2 ORDER BY dateline DESC');
+
+$orderids = array();
+foreach($orders as &$o){
+	$orderids[] = $o['id'];
+}
+unset($o);
+$orderids = implode(',', $orderids);
+
+$details = $db->fetch_all("SELECT p.name,d.subtype,d.amount,d.amountunit,d.number,d.orderid
+	FROM {$tpre}orderdetail d
+		LEFT JOIN {$tpre}product p ON p.id=d.productid
+	WHERE d.orderid IN ($orderids)");
+
+$order_details = array();
+foreach($details as &$d){
+	$order_details[$d['orderid']][] = $d['name'].(!empty($d['subtype']) ? '('.$d['subtype'].')' : '').' '.($d['amount'] * $d['number']).$d['amountunit'];
+}
+unset($d);
+
+$addresses = $db->fetch_all("SELECT o.*,c.name componentname
+	FROM {$tpre}orderaddresscomponent o
+		LEFT JOIN {$tpre}addresscomponent c ON c.id=o.componentid
+	WHERE o.orderid IN ($orderids)");
+
+$order_addresses = array();
+foreach($addresses as &$a){
+	$order_addresses[$a['orderid']][$a['formatid']] = $a['componentname'];
+}
+unset($a);
+
+foreach($orders as &$o){
+	$o['items'] = implode('<br />', $order_details[$o['id']]);
+	$o['priceunit'] = Product::PriceUnits($o['priceunit']);
+	$o['address'] = &$order_addresses[$o['id']];
+}
+unset($o);
+
 include view('order');
 
 ?>
