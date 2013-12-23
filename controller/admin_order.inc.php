7d19698ac6bd8dd94a2bea933fe0afe386760027
Takashiro <takashiro@qq.com>
2013-12-23 20:26:46 +0800

    Advanced search for orders
    Signed-off-by: Takashiro <takashiro@qq.com>

--- controller/admin_order.inc.php
+++ controller/admin_order.inc.php
@@ -2,180 +2,227 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-$actions = array('list', 'mark_unsorted', 'mark_sorted', 'mark_delivering', 'mark_received', 'mark_rejected', 'print', 'delete');
+$actions = array('list', 'mark_unsorted', 'mark_sorted', 'mark_delivering', 'mark_received', 'mark_rejected', 'print', 'delete', 'search');
 $action = isset($_REQUEST['action']) && in_array($_REQUEST['action'], $actions) ? $_REQUEST['action'] : $actions[0];
 
 switch($action){
-	case 'list':
-		$condition = array();
+	case 'list':case 'search':
+		if($action == 'list'){
+			$condition = array();
 
-		if(!empty($_POST['display_status']) && is_array($_POST['display_status'])){
-			$display_status = $_POST['display_status'];
-		}elseif(!empty($_GET['display_status'])){
-			$display_status = array();
-			foreach(explode(',', $_GET['display_status']) as $status){
-				$display_status[$status] = true;
-			}
-		}else{
-			$display_status = Order::$Status;
-		}
-
-		if(!$_G['admin']->hasPermission('order_sort')){
-			unset($display_status[Order::Unsorted]);
-		}
-
-		if(!$_G['admin']->hasPermission('order_deliver')){
-			unset($display_status[Order::Sorted], $display_status[Order::Delivering]);
-		}
-
-		if(!empty($_REQUEST['orderid'])){
-			$condition[] = 'id='.intval($_REQUEST['orderid']);
-			$display_status[Order::Received] = $display_status[Order::Rejected] = true;
-
-			$time_start = '';
-			$time_end = '';
-		}else{
-			if(isset($_REQUEST['time_start'])){
-				$time_start = empty($_REQUEST['time_start']) ? '' : rstrtotime($_REQUEST['time_start']);
+			if(!empty($_POST['display_status']) && is_array($_POST['display_status'])){
+				$display_status = $_POST['display_status'];
+			}elseif(!empty($_GET['display_status'])){
+				$display_status = array();
+				foreach(explode(',', $_GET['display_status']) as $status){
+					$display_status[$status] = true;
+				}
 			}else{
-				$time_start = rmktime(17, 0, 0, rdate(TIMESTAMP, 'm'), rdate(TIMESTAMP, 'd') - 1, rdate(TIMESTAMP, 'Y'));
+				$display_status = Order::$Status;
 			}
-			if(isset($_REQUEST['time_end'])){
-				$time_end = empty($_REQUEST['time_end']) ? '' : rstrtotime($_REQUEST['time_end']);
+
+			if(!$_G['admin']->hasPermission('order_sort')){
+				unset($display_status[Order::Unsorted]);
+			}
+
+			if(!$_G['admin']->hasPermission('order_deliver')){
+				unset($display_status[Order::Sorted], $display_status[Order::Delivering]);
+			}
+
+			if(!empty($_REQUEST['orderid'])){
+				$condition[] = 'id='.intval($_REQUEST['orderid']);
+				$display_status[Order::Received] = $display_status[Order::Rejected] = true;
+
+				$time_start = '';
+				$time_end = '';
 			}else{
-				$time_end = TIMESTAMP;
-			}
-			
-			if($time_start !== ''){
-				$condition[] = 'o.dateline>='.$time_start;
-				$time_start = rdate($time_start, 'Y-m-d H:i');
-			}
-
-			if($time_end !== ''){
-				$condition[] = 'o.dateline<='.$time_end;
-				$time_end = rdate($time_end, 'Y-m-d H:i');
-			}
-		}
-		$display_status = array_keys($display_status);
-		$condition[] = 'o.status IN ('.implode(',', $display_status).')';
-
-		$limitation_condition = array();
-		foreach($_G['admin']->getLimitations() as $componentid){
-			$limitation_condition[] = "o.id IN (SELECT orderid FROM {$tpre}orderaddresscomponent WHERE componentid=$componentid)";
-		}
-
-		if($limitation_condition){
-			$condition[] = '('.implode(' OR ', $limitation_condition).')';
-		}
-
-		$order_address = array();
-
-		$componentid = 0;
-		if(!empty($_REQUEST['delivery_address'])){
-			$delivery_address = $_REQUEST['delivery_address'];
-			$address = explode(',', $delivery_address);
-			foreach($address as $format_order => $id){
-				$id = intval($id);
-				if($id <= 0){
-					$format_order--;
-					break;
+				if(isset($_REQUEST['time_start'])){
+					$time_start = empty($_REQUEST['time_start']) ? '' : rstrtotime($_REQUEST['time_start']);
+				}else{
+					$time_start = rmktime(17, 0, 0, rdate(TIMESTAMP, 'm'), rdate(TIMESTAMP, 'd') - 1, rdate(TIMESTAMP, 'Y'));
+				}
+				if(isset($_REQUEST['time_end'])){
+					$time_end = empty($_REQUEST['time_end']) ? '' : rstrtotime($_REQUEST['time_end']);
+				}else{
+					$time_end = $time_start + 24 * 3600;
+				}
+				
+				if($time_start !== ''){
+					$condition[] = 'o.dateline>='.$time_start;
+					$time_start = rdate($time_start, 'Y-m-d H:i');
 				}
 
-				$componentid = $id;
+				if($time_end !== ''){
+					$condition[] = 'o.dateline<='.$time_end;
+					$time_end = rdate($time_end, 'Y-m-d H:i');
+				}
+			}
+			$display_status = array_keys($display_status);
+			$condition[] = 'o.status IN ('.implode(',', $display_status).')';
+
+			$limitation_condition = array();
+			foreach($_G['admin']->getLimitations() as $componentid){
+				$limitation_condition[] = "o.id IN (SELECT orderid FROM {$tpre}orderaddresscomponent WHERE componentid=$componentid)";
 			}
 
-			if($format_order >= 0){
-				$order_address[] = $componentid;
+			if($limitation_condition){
+				$condition[] = '('.implode(' OR ', $limitation_condition).')';
+			}
+
+			$order_address = array();
+
+			$delivery_address = array();
+			if(!empty($_REQUEST['delivery_address']) && is_array($_REQUEST['delivery_address'])){
+				$delivery_address = &$_REQUEST['delivery_address'];
+				foreach($delivery_address as $address){
+					$componentid = NULL;
+					$address = explode(',', $address);
+					foreach($address as $format_order => $id){
+						$id = intval($id);
+						if($id <= 0){
+							$format_order--;
+							break;
+						}
+
+						$componentid = $id;
+					}
+
+					if($format_order >= 0 && $componentid !== NULL){
+						$order_address[] = $componentid;
+					}
+				}
+			}
+
+			if($order_address){
+				$order_address = array_unique($order_address);
+				$condition[] = 'o.id IN (SELECT orderid FROM '.$tpre.'orderaddresscomponent WHERE componentid IN ('.implode(',', $order_address).'))';
+			}
+
+			if(!empty($_REQUEST['userid'])){
+				$userid = intval($_REQUEST['userid']);
+				$condition[] = 'o.userid='.$userid;
+			}else{
+				$userid = '';
+			}
+
+			if(!empty($_REQUEST['addressee'])){
+				$addressee = trim($_REQUEST['addressee']);
+				$condition[] = 'o.addressee LIKE \'%'.$addressee.'%\'';
+			}else{
+				$addressee = '';
+			}
+
+			if(!empty($_REQUEST['mobile'])){
+				$mobile = trim($_REQUEST['mobile']);
+				$condition[] = 'o.mobile=\''.$mobile.'\'';
+			}else{
+				$mobile = '';
+			}
+
+			if(!empty($_REQUEST['administrator'])){
+				$administrator = trim($_REQUEST['administrator']);
+				$limitation = $db->result_first("SELECT limitation FROM {$tpre}administrator WHERE account='$administrator'");
+				$limitation = explode(',', $limitation);
+				foreach($limitation as &$limitation_i){
+					$limitation_i = intval($limitation_i);
+				}
+				unset($limitation_i);
+				$limitation = array_unique($limitation);
+
+				if($limitation){
+					$limitation = implode(',', $limitation);
+					$condition[] = 'o.id IN (SELECT orderid FROM '.$tpre.'orderaddresscomponent WHERE componentid IN ('.$limitation.'))';
+				}
+			}else{
+				$administrator = '';
+			}
+
+			$condition = implode(' AND ', $condition);
+			
+			$stat = array(
+				'statonly' => !empty($_REQUEST['stat']['statonly']),
+				'totalprice' => !empty($_REQUEST['stat']['totalprice']),
+				'item' => !empty($_REQUEST['stat']['item']),
+			);
+
+			$limit = 20;
+			$offset = ($page - 1) * $limit;
+			$pagenum = $db->result_first("SELECT COUNT(*) FROM {$tpre}order o WHERE $condition");
+			if(!$stat['statonly']){
+				$orders = $db->fetch_all("SELECT *,(SELECT COUNT(*) FROM {$tpre}order WHERE userid=o.userid AND dateline<o.dateline) AS ordernum
+					FROM {$tpre}order o
+					WHERE $condition ORDER BY o.dateline LIMIT $offset,$limit");
+			}else{
+				$orders = array();
+			}
+
+			$statdata = array();
+			if($stat['totalprice']){
+				$statdata['totalprice'] = $db->fetch_all("SELECT SUM(totalprice) AS price,priceunit FROM {$tpre}order o WHERE $condition GROUP BY priceunit");
+				foreach($statdata['totalprice'] as &$total){
+					$total['priceunit'] = Product::PriceUnits($total['priceunit']);
+				}
+				unset($total);
+			}else{
+				$statdata['totalprice'] = array();
+			}
+
+			if($stat['item']){
+				$statdata['item'] = $db->fetch_all("SELECT d.productname,d.subtype,d.amountunit,SUM(d.amount*d.number) AS num,o.priceunit,SUM(d.subtotal) AS totalprice
+					FROM {$tpre}orderdetail d
+						LEFT JOIN {$tpre}order o ON d.orderid=o.id
+					WHERE $condition
+					GROUP BY d.productname,d.subtype,d.amountunit,o.priceunit");
+
+				foreach($statdata['item'] as &$item){
+					$item['priceunit'] = Product::PriceUnits($item['priceunit']);
+				}
+				unset($item);
+			}else{
+				$statdata['item'] = array();
+			}
+
+			if($orders){
+				$orderids = array();
+				foreach($orders as &$o){
+					$orderids[] = $o['id'];
+				}
+				unset($o);
+
+				$orderids = implode(',', $orderids);
+
+				$details = $db->fetch_all("SELECT d.productname,d.subtype,d.amount,d.amountunit,d.number,d.orderid
+					FROM {$tpre}orderdetail d
+					WHERE d.orderid IN ($orderids)");
+
+				$order_details = array();
+				foreach($details as &$d){
+					$order_details[$d['orderid']][] = $d['productname'].(!empty($d['subtype']) ? '('.$d['subtype'].')' : '').' '.($d['amount'] * $d['number']).$d['amountunit'];
+				}
+				unset($d);
+
+				$addresses = $db->fetch_all("SELECT o.*,c.name componentname
+					FROM {$tpre}orderaddresscomponent o
+						LEFT JOIN {$tpre}addresscomponent c ON c.id=o.componentid
+					WHERE o.orderid IN ($orderids)");
+
+				$order_addresses = array();
+				foreach($addresses as &$a){
+					$order_addresses[$a['orderid']][$a['formatid']] = $a['componentname'];
+				}
+				unset($a);
+
+				foreach($orders as &$o){
+					$o['items'] = implode('<br />', $order_details[$o['id']]);
+					$o['priceunit'] = Product::PriceUnits($o['priceunit']);
+					$o['address'] = &$order_addresses[$o['id']];
+				}
+				unset($o);
 			}
 		}else{
-			$delivery_address = '0,0';
-		}
-
-		foreach($order_address as $componentid){
-			$condition[] = "o.id IN (SELECT orderid FROM {$tpre}orderaddresscomponent WHERE componentid=$componentid)";
-		}
-
-		$condition = implode(' AND ', $condition);
-		
-		$stat = array(
-			'statonly' => !empty($_REQUEST['stat']['statonly']),
-			'totalprice' => !empty($_REQUEST['stat']['totalprice']),
-			'item' => !empty($_REQUEST['stat']['item']),
-		);
-
-		$limit = 20;
-		$offset = ($page - 1) * $limit;
-		$pagenum = $db->result_first("SELECT COUNT(*) FROM {$tpre}order o WHERE $condition");
-		if(!$stat['statonly']){
-			$orders = $db->fetch_all("SELECT *,(SELECT COUNT(*) FROM {$tpre}order WHERE userid=o.userid AND dateline<o.dateline) AS ordernum
-				FROM {$tpre}order o
-				WHERE $condition ORDER BY o.dateline LIMIT $offset,$limit");
-		}else{
-			$orders = array();
-		}
-
-		$statdata = array();
-		if($stat['totalprice']){
-			$statdata['totalprice'] = $db->fetch_all("SELECT SUM(totalprice) AS price,priceunit FROM {$tpre}order o WHERE $condition GROUP BY priceunit");
-			foreach($statdata['totalprice'] as &$total){
-				$total['priceunit'] = Product::PriceUnits($total['priceunit']);
-			}
-			unset($total);
-		}else{
-			$statdata['totalprice'] = array();
-		}
-
-		if($stat['item']){
-			$statdata['item'] = $db->fetch_all("SELECT d.productname,d.subtype,d.amountunit,SUM(d.amount*d.number) AS num,o.priceunit,SUM(d.subtotal) AS totalprice
-				FROM {$tpre}orderdetail d
-					LEFT JOIN {$tpre}order o ON d.orderid=o.id
-				WHERE $condition
-				GROUP BY d.productname,d.subtype,d.amountunit,o.priceunit");
-
-			foreach($statdata['item'] as &$item){
-				$item['priceunit'] = Product::PriceUnits($item['priceunit']);
-			}
-			unset($item);
-		}else{
-			$statdata['item'] = array();
-		}
-
-		if($orders){
-			$orderids = array();
-			foreach($orders as &$o){
-				$orderids[] = $o['id'];
-			}
-			unset($o);
-
-			$orderids = implode(',', $orderids);
-
-			$details = $db->fetch_all("SELECT d.productname,d.subtype,d.amount,d.amountunit,d.number,d.orderid
-				FROM {$tpre}orderdetail d
-				WHERE d.orderid IN ($orderids)");
-
-			$order_details = array();
-			foreach($details as &$d){
-				$order_details[$d['orderid']][] = $d['productname'].(!empty($d['subtype']) ? '('.$d['subtype'].')' : '').' '.($d['amount'] * $d['number']).$d['amountunit'];
-			}
-			unset($d);
-
-			$addresses = $db->fetch_all("SELECT o.*,c.name componentname
-				FROM {$tpre}orderaddresscomponent o
-					LEFT JOIN {$tpre}addresscomponent c ON c.id=o.componentid
-				WHERE o.orderid IN ($orderids)");
-
-			$order_addresses = array();
-			foreach($addresses as &$a){
-				$order_addresses[$a['orderid']][$a['formatid']] = $a['componentname'];
-			}
-			unset($a);
-
-			foreach($orders as &$o){
-				$o['items'] = implode('<br />', $order_details[$o['id']]);
-				$o['priceunit'] = Product::PriceUnits($o['priceunit']);
-				$o['address'] = &$order_addresses[$o['id']];
-			}
-			unset($o);
+			$display_status = array_keys(Order::$Status);
+			$time_start = rmktime(17, 0, 0, rdate(TIMESTAMP, 'm'), rdate(TIMESTAMP, 'd') - 1, rdate(TIMESTAMP, 'Y'));
+			$time_end = $time_start + 24 * 3600;
 		}
 
 		$address_format = Address::Format();
@@ -235,7 +282,7 @@ switch($action){
 			$available_status[$status] = true;
 		}
 
-		include view('order_list');
+		include view('order_'.$action);
 	break;
 
 	case 'mark_unsorted':
