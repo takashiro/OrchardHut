ed5bb0c93b1e4f1ab3b1fc0e2f089bb1f88aa1ff
Takashiro <takashiro@qq.com>
2015-02-20 15:18:31 +0800

    Add time selection to sale report

--- controller/admin_salereport.inc.php
+++ controller/admin_salereport.inc.php
@@ -6,11 +6,37 @@ $template_formats = array('html', 'csv');
 $format = &$_GET['format'];
 in_array($format, $template_formats) || $format = $template_formats[0];
 
-$items = $db->fetch_all("SELECT productid,productname,amountunit,SUM(`amount`*`number`) AS amount,subtype,SUM(subtotal) AS totalprice
-	FROM {$tpre}orderdetail
-	WHERE state=0
+$condition = array('d.state=0');
+
+//下单起始时间
+if(isset($_REQUEST['time_start'])){
+	$time_start = empty($_REQUEST['time_start']) ? '' : rstrtotime($_REQUEST['time_start']);
+}else{
+	$time_start = rmktime(0, 0, 0, rdate(TIMESTAMP, 'm'), 1, rdate(TIMESTAMP, 'Y'));
+}
+if($time_start){
+	$condition[] = 'o.dateline>='.$time_start;
+}
+
+//下单截止时间
+if(isset($_REQUEST['time_end'])){
+	$time_end = empty($_REQUEST['time_end']) ? '' : rstrtotime($_REQUEST['time_end']);
+}else{
+	$time_end = rmktime(23, 59, 59, rdate($time_start, 'm') + 1, rdate($time_start, 'd') - 1, rdate($time_start, 'Y'));
+}
+if($time_end){
+	$condition[] = 'o.dateline<='.$time_end;
+}
+
+$condition = implode(' AND ', $condition);
+
+$items = $db->fetch_all("SELECT d.productid,d.productname,d.amountunit,SUM(d.amount*d.number) AS amount,d.subtype,SUM(d.subtotal) AS totalprice
+	FROM {$tpre}orderdetail d
+		LEFT JOIN {$tpre}order o ON o.id=d.orderid
+	WHERE $condition
 	GROUP BY productid");
 
+//总计
 $statistic = array(
 	'totalprice' => 0,
 );
@@ -18,6 +44,8 @@ foreach($items as $item){
 	$statistic['totalprice'] += $item['totalprice'];
 }
 
+$time_start = rdate($time_start);
+$time_end = rdate($time_end);
 include view('salereport_'.$format);
 
 ?>
--- js/admin_order_list.js
+++ js/admin_order_list.js
@@ -1,22 +1,4 @@
 $(function(){
-	$('input.datetime').datetimepicker({
-		lang : 'cn',
-		i18n : {
-			cn : {
-				months : [
-					'1月','2月','3月','4月',
-					'5月','6月','7月','8月',
-					'9月','10月','11月','12月',
-				],
-				dayOfWeek : [
-					"日", "一", "二", "三",
-					"四", "五", "六",
-				]
-			}
-		},
-		format : 'Y-m-d H:i',
-	});
-
 	$('#orderlist').on('click', '.mark_sorted, .mark_delivering, .mark_in_delivery_point, .mark_received, .mark_rejected', function(e){
 		var a = $(e.target);
 		var href = a.attr('href');
