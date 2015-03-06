15b0d8d0e24d8577e8e40c878a4b89f35dc8270a
Takashiro <takashiro@qq.com>
2015-03-06 11:39:18 +0800

    Add weixin language pack

---
+++ view/admin/default/weixin.lang.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+	'your_order_just_arrived_in_delivery_point' => '您的订单已经到达自提点。',
+	'your_order_is_being_delivered' => '您的订单正在派送中。',
+	'deliverer_is' => '配送员：',
+	'click_here_for_detail' => '点此查看详情',
+);
+
+?>
