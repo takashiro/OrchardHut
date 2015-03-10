709baafc474ee2a040f4fbef0dc999ec6b55e059
Takashiro <takashiro@qq.com>
2015-03-10 16:34:20 +0800

    Simplify words of trade status

--- view/user/default/common.lang.php
+++ view/user/default/common.lang.php
@@ -8,14 +8,14 @@ return array(
 	'order_in_delivery_point' => '待自提',
 	'order_received' => '已签收',
 	'order_rejected' => '已拒收',
-	'order_paidwithcash' => '货到付款(现金)',
+	'order_paidwithcash' => '货到付现',
 	'order_paidonline' => '在线支付',
 
-	'alipay_waitbuyerpay' => '等待付款',
-	'alipay_tradeclosed' => '交易关闭',
-	'alipay_tradesuccess' => '交易成功',
-	'alipay_tradepending' => '等待收款',
-	'alipay_tradefinished' => '交易结束',
+	'alipay_waitbuyerpay' => '待付款',
+	'alipay_tradeclosed' => '关闭',
+	'alipay_tradesuccess' => '成功',
+	'alipay_tradepending' => '待收款',
+	'alipay_tradefinished' => '结束',
 
 	'mobile' => '手机',
 );
