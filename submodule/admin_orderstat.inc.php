88d47a0302db58ec988d98841b44fa0481b12336
Takashiro <takashiro@qq.com>
2015-05-15 13:19:55 +0800

    加入订单统计功能

---
+++ submodule/admin_orderstat.inc.php
@@ -0,0 +1,125 @@
+<?php
+
+if(!defined('IN_ADMINCP')) exit('access denied');
+
+$condition = array();
+
+//时间条件
+$time_start = null;
+if(isset($_REQUEST['time_start'])){
+	$time_start = rstrtotime($_REQUEST['time_start']);
+	$condition[] = 'o.dateline>='.$time_start;
+}else{
+	$time_start = rmktime(0, 0, 0, rdate(TIMESTAMP, 'm'), rdate(TIMESTAMP, 'd') - 1, rdate(TIMESTAMP, 'Y'));
+
+	//根据截单时间调整时分秒
+	$deliverytimes = DeliveryTime::FetchAllEffective();
+	usort($deliverytimes, function($t1, $t2){
+		return $t1['deadline'] > $t2['deadline'];
+	});
+	$dt = current($deliverytimes);
+	$time_start += $dt['deadline'];
+	unset($deliverytimes, $dt);
+}
+$time_end = null;
+if(isset($_REQUEST['time_end'])){
+	$time_end = rstrtotime($_REQUEST['time_end']);
+	$condition[] = 'o.dateline<='.$time_end;
+}else{
+	$time_end = $time_start + 24 * 3600;
+}
+
+//过滤商品类型
+$product_type = array();
+if(isset($_REQUEST['product_type']) && is_array($_REQUEST['product_type'])){
+	$types = array();
+	foreach($_REQUEST['product_type'] as $type_id => $checked){
+		$product_type[$type_id] = true;
+		$types[] = $type_id;
+	}
+
+	$types && $condition[] = 'p.type IN ('.implode(',', $types).')';
+}else{
+	$product_type = Product::Types();
+	foreach($product_type as &$checked){
+		$checked = false;
+	}
+	unset($checked);
+}
+
+//过滤订单状态
+$available_status = Order::$Status;
+if(!$_G['admin']->hasPermission('order_sort')){
+	unset($available_status[Order::Unsorted]);
+}
+if(!$_G['admin']->hasPermission('order_deliver')){
+	unset($available_status[Order::Sorted], $available_status[Order::Delivering]);
+}
+foreach($available_status as &$checked){
+	$checked = false;
+}
+unset($checked);
+if(isset($_REQUEST['display_status']) && is_array($_REQUEST['display_status'])){
+	foreach($_REQUEST['display_status'] as $status_id => $checked){
+		$available_status[$status_id] = !empty($checked);
+	}
+}
+
+$display_status = array();
+foreach($available_status as $status_id => $checked){
+	$checked && $display_status[] = $status_id;
+}
+if($display_status){
+	$condition[] = 'o.status IN ('.implode(',', $display_status).')';
+}else{
+	$condition[] = '0';
+}
+
+//生成查询条件子句
+if($condition){
+	$condition = implode(' AND ', $condition);
+}else{
+	$condition = '1';
+}
+
+$address_tables = array();
+$address_format = Address::Format();
+$cascade_level = count($address_format);
+$i = $cascade_level;
+$address_tables[] = "LEFT JOIN {$tpre}addresscomponent a{$i} ON a{$i}.id=o.addressid";
+for($i = $cascade_level - 1; $i >= 1; $i--){
+	$j = $i + 1;
+	$address_tables[] = "LEFT JOIN {$tpre}addresscomponent a{$i} ON a{$i}.id=a{$j}.parentid";
+}
+$address_tables = implode(' ', $address_tables);
+
+$item_list = $db->fetch_all("SELECT a1.id AS topaddressid, a1.name AS topaddressname,
+		d.productid,d.productname,d.subtype, SUM(d.amount*d.number) AS totalnum
+	FROM {$tpre}orderdetail d
+		LEFT JOIN {$tpre}product p ON p.id=d.productid
+		LEFT JOIN {$tpre}order o ON o.id=d.orderid
+		$address_tables
+	WHERE $condition
+	GROUP BY a1.id,d.productid,d.subtype");
+
+$top_address_list = array();
+$product_list = array();
+$stat_list = array();
+foreach($item_list as $item){
+	$stat_list[$item['productid']][$item['subtype']][$item['topaddressid']] = $item;
+	$top_address_list[$item['topaddressid']] = $item['topaddressname'];
+	$product_list[$item['productid']] = $item['productname'];
+}
+
+if($time_start !== null){
+	$time_start = rdate($time_start);
+}
+if($time_end !== null){
+	$time_end = rdate($time_end);
+}
+
+$formats = array('html', 'csv');
+$format = isset($_REQUEST['format']) && in_array($_REQUEST['format'], $formats) ? $_REQUEST['format'] : 'html';
+include view('orderstat_'.$format);
+
+?>
---
+++ view/admin/default/orderstat_csv.php
@@ -0,0 +1,34 @@
+<?php
+
+if(!defined('IN_ADMINCP')) exit('access denied');
+
+rheader('Cache-Control: no-cache, must-revalidate');
+rheader('Content-Type: application/octet-stream');
+rheader('Content-Disposition: attachment; filename="'.$_CONFIG['sitename'].'订单分拣报表('.rdate(TIMESTAMP, 'Y-m-d His').').csv"');
+
+//UTF-8 BOM
+echo chr(0xEF), chr(0xBB), chr(0xBF);
+
+//表头
+echo '产品';
+foreach($top_address_list as $address){
+	echo ',', $address;
+}
+echo "\r\n";
+
+//统计结果
+foreach($stat_list as $product_id => $subtypes){
+	foreach($subtypes as $subtype => $items){
+		echo $product_list[$product_id];
+		if(!empty($subtype)){
+			echo '(', $subtype, '),';
+		}
+
+		foreach($top_address_list as $address_id => $address_name){
+			echo ',', $items[$address_id]['totalnum'];
+		}
+		echo "\r\n";
+	}
+}
+
+?>
