73d94e7dcd1318bb9db2d8a702cf90e585e956f3
Takashiro <takashiro@qq.com>
2015-06-25 16:25:12 +0800

    充值奖励模块更新

--- submodule/admin/prepaidreward.inc.php
+++ submodule/admin/prepaidreward.inc.php
@@ -23,63 +23,70 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-if($_G['admincp']['mode'] == 'permission'){
-	return array();
-}
+class PrepaidRewardModule extends AdminControlPanelModule{
 
-$table = $db->select_table('prepaidreward');
+	public function editAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
 
-$action = &$_GET['action'];
-switch($action){
-case 'edit':
-	$prepaidreward = array();
+		$table = $db->select_table('prepaidreward');
+		$prepaidreward = array();
 
-	foreach(array('minamount', 'maxamount', 'reward') as $var){
-		if(isset($_POST[$var])){
-			$prepaidreward[$var] = floatval($_POST[$var]);
+		foreach(array('minamount', 'maxamount', 'reward') as $var){
+			if(isset($_POST[$var])){
+				$prepaidreward[$var] = floatval($_POST[$var]);
+			}
 		}
-	}
 
-	foreach(array('etime_start', 'etime_end') as $var){
-		isset($_POST[$var]) && $prepaidreward[$var] = rstrtotime($_POST[$var]);
-	}
-
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	if($id > 0){
-		$table->update($prepaidreward, 'id='.$id);
-		echo $db->affected_rows;
-	}else{
-		$table->insert($prepaidreward);
-
-		$prepaidreward['id'] = $table->insert_id();
 		foreach(array('etime_start', 'etime_end') as $var){
-			isset($prepaidreward[$var]) && $prepaidreward[$var] = rdate($prepaidreward[$var]);
+			isset($_POST[$var]) && $prepaidreward[$var] = rstrtotime($_POST[$var]);
 		}
 
-		echo json_encode($prepaidreward);
-	}
-	exit;
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		if($id > 0){
+			$table->update($prepaidreward, 'id='.$id);
+			echo $db->affected_rows;
+		}else{
+			$table->insert($prepaidreward);
 
-case 'delete':
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	if($id > 0){
-		$table->delete('id='.$id);
-		echo $db->affected_rows;
-	}else{
-		echo 0;
-	}
-	exit;
+			$prepaidreward['id'] = $table->insert_id();
+			foreach(array('etime_start', 'etime_end') as $var){
+				isset($prepaidreward[$var]) && $prepaidreward[$var] = rdate($prepaidreward[$var]);
+			}
 
-default:
-	$prepaidrewards = $table->fetch_all('*');
-	foreach($prepaidrewards as &$prepaidreward){
-		foreach(array('etime_start', 'etime_end') as $var){
-			$prepaidreward[$var] = rdate($prepaidreward[$var]);
+			echo json_encode($prepaidreward);
 		}
-	}
-	unset($prepaidreward);
 
-	include view('prepaidreward');
+		exit;
+	}
+
+	public function deleteAction(){
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		if($id > 0){
+			global $db;
+			$table = $db->select_table('prepaidreward');
+			$table->delete('id='.$id);
+			echo $db->affected_rows;
+		}else{
+			echo 0;
+		}
+		exit;
+	}
+
+	public function defaultAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
+
+		$table = $db->select_table('prepaidreward');
+		$prepaidrewards = $table->fetch_all('*');
+		foreach($prepaidrewards as &$prepaidreward){
+			foreach(array('etime_start', 'etime_end') as $var){
+				$prepaidreward[$var] = rdate($prepaidreward[$var]);
+			}
+		}
+		unset($prepaidreward);
+
+		include view('prepaidreward');
+	}
+
 }
 
 ?>
