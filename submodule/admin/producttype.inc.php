e22492ce2c4202e1fe15d36d4e446b005f084e97
Takashiro <takashiro@qq.com>
2015-06-25 16:27:49 +0800

    商品类型模块更新

--- submodule/admin/producttype.inc.php
+++ submodule/admin/producttype.inc.php
@@ -23,56 +23,58 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-if($_G['admincp']['mode'] == 'permission'){
-	return array();
-}
+class ProductTypeModule extends AdminControlPanelModule{
 
-$actions = array('list', 'edit', 'delete');
-$action = !empty($_GET['action']) && in_array($_GET['action'], $actions) ? $_GET['action'] : $actions[0];
+	public function editAction(){
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		$producttype = array();
+		if(isset($_POST['name'])){
+			$producttype['name'] = trim($_POST['name']);
+		}
+		if(isset($_POST['displayorder'])){
+			$producttype['displayorder'] = intval($_POST['displayorder']);
+		}
 
 
-$table = $db->select_table('producttype');
-$action = &$_GET['action'];
-switch($action){
-case 'edit':
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	$producttype = array();
-	if(isset($_POST['name'])){
-		$producttype['name'] = trim($_POST['name']);
-	}
-	if(isset($_POST['displayorder'])){
-		$producttype['displayorder'] = intval($_POST['displayorder']);
-	}
+		global $db;
+		$table = $db->select_table('producttype');
+		if($id > 0){
+			$table->update($producttype, 'id='.$id);
+			$producttype['id'] = $id;
+		}else{
+			$table->insert($producttype);
+			$producttype['id'] = $table->insert_id();
+		}
 
-	if($id > 0){
-		$table->update($producttype, 'id='.$id);
-		$producttype['id'] = $id;
-	}else{
-		$table->insert($producttype);
-		$producttype['id'] = $table->insert_id();
-	}
-
-	Product::RefreshCache();
-
-	echo json_encode($producttype);
-	exit;
-	break;
-case 'delete':
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	if($id > 0){
-		$table->delete('id='.$id);
 		Product::RefreshCache();
 
-		echo $db->affected_rows;
-	}else{
-		echo 0;
+		echo json_encode($producttype);
+		exit;
 	}
-	exit;
-	break;
-default:
-	$product_types = $table->fetch_all('*', '1 ORDER BY displayorder');
+
+	public function deleteAction(){
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		if($id > 0){
+			global $db;
+			$table = $db->select_table('producttype');
+			$table->delete('id='.$id);
+			Product::RefreshCache();
+
+			echo $db->affected_rows;
+		}else{
+			echo 0;
+		}
+	}
+
+	public function defaultAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
+
+		$table = $db->select_table('producttype');
+		$product_types = $table->fetch_all('*', '1 ORDER BY displayorder');
+
+		include view('producttype');
+	}
 }
 
-include view('producttype');
 
 ?>
