b54a2852260d449673efeeba8414cfc35bf05bc8
Takashiro <takashiro@qq.com>
2015-06-25 16:29:41 +0800

    商品单位管理模块更新

--- submodule/admin/productunit.inc.php
+++ submodule/admin/productunit.inc.php
@@ -23,57 +23,63 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-if($_G['admincp']['mode'] == 'permission'){
-	return array();
-}
 
-$actions = array('list', 'edit', 'delete');
-$action = !empty($_GET['action']) && in_array($_GET['action'], $actions) ? $_GET['action'] : $actions[0];
+class ProductUnitModule extends AdminControlPanelModule{
 
+	public function editAction(){
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		$unit = array();
+		if(isset($_POST['name'])){
+			$unit['name'] = trim($_POST['name']);
+		}
+		if(isset($_POST['type'])){
+			$unit['type'] = intval($_POST['type']);
+			$unit['type'] = $unit['type'] == 1 ? 1 : 2;
+		}
+		if(isset($_POST['hidden'])){
+			$unit['hidden'] = empty($_POST['hidden']) ? 0 : 1;
+		}
 
-$table = $db->select_table('productunit');
-$action = &$_GET['action'];
-switch($action){
-case 'edit':
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	$unit = array();
-	if(isset($_POST['name'])){
-		$unit['name'] = trim($_POST['name']);
-	}
-	if(isset($_POST['type'])){
-		$unit['type'] = intval($_POST['type']);
-		$unit['type'] = $unit['type'] == 1 ? 1 : 2;
-	}
+		global $db;
+		$table = $db->select_table('productunit');
+		if($id > 0){
+			$table->update($unit, 'id='.$id);
+			$unit['id'] = $id;
+		}else{
+			$table->insert($unit);
+			$unit['id'] = $table->insert_id();
+		}
 
-	if($id > 0){
-		$table->update($unit, 'id='.$id);
-		$unit['id'] = $id;
-	}else{
-		$table->insert($unit);
-		$unit['id'] = $table->insert_id();
-	}
-
-	Product::RefreshCache();
-
-	echo json_encode($unit);
-	exit;
-	break;
-case 'delete':
-	$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
-	if($id > 0){
-		$table->delete('id='.$id);
 		Product::RefreshCache();
 
-		echo $db->affected_rows;
-	}else{
-		echo 0;
+		echo json_encode($unit);
+		exit;
 	}
-	exit;
-	break;
-default:
-	$product_units = $table->fetch_all('*', '1 ORDER BY type,id');
-}
 
-include view('productunit');
+	public function deleteAction(){
+		$id = isset($_REQUEST['id']) ? intval($_REQUEST['id']) : 0;
+		if($id > 0){
+			global $db;
+			$table = $db->select_table('productunit');
+			$table->delete('id='.$id);
+			Product::RefreshCache();
+
+			echo $db->affected_rows;
+		}else{
+			echo 0;
+		}
+		exit;
+	}
+
+	public function defaultAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
+
+		$table = $db->select_table('productunit');
+		$product_units = $table->fetch_all('*', '1 ORDER BY type,id');
+
+		include view('productunit');
+	}
+
+}
 
 ?>
