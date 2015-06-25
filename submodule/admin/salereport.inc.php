8a84dd5c5d5765b8b1ec3df3fd89486d3f5f28d9
Takashiro <takashiro@qq.com>
2015-06-25 16:31:06 +0800

    销售报表模块更新

--- submodule/admin/salereport.inc.php
+++ submodule/admin/salereport.inc.php
@@ -23,69 +23,73 @@
 
 if(!defined('IN_ADMINCP')) exit('access denied');
 
-if($_G['admincp']['mode'] == 'permission'){
-	return array();
-}
+class SaleReportModule extends AdminControlPanelModule{
 
-$template_formats = array('html', 'csv');
-$format = &$_GET['format'];
-in_array($format, $template_formats) || $format = $template_formats[0];
+	public function defaultAction(){
+		extract($GLOBALS, EXTR_SKIP | EXTR_REFS);
 
-$condition = array('d.state=0');
+		$template_formats = array('html', 'csv');
+		$format = &$_GET['format'];
+		in_array($format, $template_formats) || $format = $template_formats[0];
 
-//下单起始时间
-if(isset($_REQUEST['time_start'])){
-	$time_start = empty($_REQUEST['time_start']) ? '' : rstrtotime($_REQUEST['time_start']);
-}else{
-	$time_start = rmktime(0, 0, 0, rdate(TIMESTAMP, 'm'), 1, rdate(TIMESTAMP, 'Y'));
-}
-if($time_start){
-	$condition[] = 'o.dateline>='.$time_start;
-}
+		$condition = array('d.state=0');
 
-//下单截止时间
-if(isset($_REQUEST['time_end'])){
-	$time_end = empty($_REQUEST['time_end']) ? '' : rstrtotime($_REQUEST['time_end']);
-}else{
-	$time_end = rmktime(23, 59, 59, rdate($time_start, 'm') + 1, rdate($time_start, 'd') - 1, rdate($time_start, 'Y'));
-}
-if($time_end){
-	$condition[] = 'o.dateline<='.$time_end;
-}
+		//下单起始时间
+		if(isset($_REQUEST['time_start'])){
+			$time_start = empty($_REQUEST['time_start']) ? '' : rstrtotime($_REQUEST['time_start']);
+		}else{
+			$time_start = rmktime(0, 0, 0, rdate(TIMESTAMP, 'm'), 1, rdate(TIMESTAMP, 'Y'));
+		}
+		if($time_start){
+			$condition[] = 'o.dateline>='.$time_start;
+		}
 
-//根据送货地址统计报表
-if(!empty($_POST['order_address'])){
-	$order_address = intval($_POST['order_address']);
-	$extension = Address::Extension($order_address);
-	if($extension){
-		$condition[] = 'o.addressid IN ('.implode(',', $extension).')';
-	}else{
-		$condition[] = '0';
+		//下单截止时间
+		if(isset($_REQUEST['time_end'])){
+			$time_end = empty($_REQUEST['time_end']) ? '' : rstrtotime($_REQUEST['time_end']);
+		}else{
+			$time_end = rmktime(23, 59, 59, rdate($time_start, 'm') + 1, rdate($time_start, 'd') - 1, rdate($time_start, 'Y'));
+		}
+		if($time_end){
+			$condition[] = 'o.dateline<='.$time_end;
+		}
+
+		//根据送货地址统计报表
+		if(!empty($_POST['order_address'])){
+			$order_address = intval($_POST['order_address']);
+			$extension = Address::Extension($order_address);
+			if($extension){
+				$condition[] = 'o.addressid IN ('.implode(',', $extension).')';
+			}else{
+				$condition[] = '0';
+			}
+		}else{
+			$order_address = 0;
+		}
+
+		$condition = implode(' AND ', $condition);
+		$items = $db->fetch_all("SELECT d.productid,d.productname,d.amountunit,SUM(d.amount*d.number) AS amount,d.subtype,SUM(d.subtotal) AS totalprice
+			FROM {$tpre}orderdetail d
+				LEFT JOIN {$tpre}order o ON o.id=d.orderid
+			WHERE $condition
+			GROUP BY productid");
+
+		//总计
+		$statistic = array(
+			'totalprice' => 0,
+		);
+		foreach($items as $item){
+			$statistic['totalprice'] += $item['totalprice'];
+		}
+
+		$time_start = rdate($time_start);
+		$time_end = rdate($time_end);
+
+		$address_components = Address::AvailableComponents();
+
+		include view('salereport_'.$format);
 	}
-}else{
-	$order_address = 0;
+
 }
-
-$condition = implode(' AND ', $condition);
-$items = $db->fetch_all("SELECT d.productid,d.productname,d.amountunit,SUM(d.amount*d.number) AS amount,d.subtype,SUM(d.subtotal) AS totalprice
-	FROM {$tpre}orderdetail d
-		LEFT JOIN {$tpre}order o ON o.id=d.orderid
-	WHERE $condition
-	GROUP BY productid");
-
-//总计
-$statistic = array(
-	'totalprice' => 0,
-);
-foreach($items as $item){
-	$statistic['totalprice'] += $item['totalprice'];
-}
-
-$time_start = rdate($time_start);
-$time_end = rdate($time_end);
-
-$address_components = Address::AvailableComponents();
-
-include view('salereport_'.$format);
 
 ?>
