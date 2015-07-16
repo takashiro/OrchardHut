359d518885ff0c611c01837eef7da6283f2e92c9
Takashiro <takashiro@qq.com>
2015-07-16 21:51:30 +0800

    添加通过关注公众号进行注册的提示

---
+++ view/user/default/js/login.js
@@ -0,0 +1,87 @@
+var action = 'login';
+var no_alert = false;
+
+$(function(){
+	$('#login_form').submit(function(e){
+		var data = {};
+		var arr = $(e.target).serializeArray();
+		for(var i = 0; i < arr.length; i++){
+			data[arr[i].name] = arr[i].value;
+		}
+
+		var invalid_message = '';
+
+		if(data.account == ''){
+			invalid_message += '请填写用户名。';
+		}else if(data.account.length < 4 || data.account.length > 15){
+			invalid_message += '用户名长度应在4 - 15个字符之间。';
+		}
+
+		if(data.password == ''){
+			invalid_message += '请填写密码。';
+		}else if(data.password.length < 6){
+			invalid_message += '密码至少6位。';
+		}
+
+		if(action == 'register'){
+			if(data.password2 == undefined || data.password2 == ''){
+				invalid_message += '请再次输入密码进行确认。';
+			}else if(data.password2 != data.password){
+				invalid_message += '您两次输入的密码不一致。';
+			}
+		}
+
+		if(invalid_message != ''){
+			e.preventDefault();
+
+			if(no_alert){
+				no_alert = false;
+			}else{
+				alert(invalid_message);
+			}
+		}
+	});
+
+	$('#login_button').click(function(){
+		if(action != 'login'){
+			action = 'login';
+			no_alert = true;
+
+			$('#action_text').html('登录');
+
+			$('.for_register').each(function(){
+				$(this).data('hidden_html', $(this).html());
+				$(this).html('');
+			});
+		}
+	});
+
+	$('#register_button').click(function(){
+		if(action != 'register'){
+			action = 'register';
+			no_alert = true;
+
+			$('#action_text').html('注册');
+			$('#login_button').hide();
+
+			$('.for_register').each(function(){
+				$(this).html($(this).data('hidden_html'));
+			});
+		}
+	});
+
+	$('.for_register').each(function(){
+		$(this).data('hidden_html', $(this).html());
+		$(this).html('');
+	});
+
+	$('a.wechat_login').click(function(){
+		if(follow_guide_page == undefined || window.navigator.userAgent.indexOf('MicroMessenger') == -1){
+			var title = '微信扫码';
+			var content = '<ol><li>扫码关注公众号：<br /><img src="./view/user/default/image/wx_qrcode.png" /></li><li>点击公众号菜单“进入商城”选项。</li></ol>';
+			popup_message(title, content);
+		}else{
+			location.href = follow_guide_page;
+		}
+	});
+});
