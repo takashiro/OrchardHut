4efbc644746232479e8c6cba90ac1c6facfe5b1f
Takashiro <takashiro@qq.com>
2015-07-17 13:31:14 +0800

    微信自定义菜单：支持所有类型的按钮

--- view/admin/default/weixin.lang.php
+++ view/admin/default/weixin.lang.php
@@ -7,6 +7,15 @@ return array(
 	'please_wait_for_the_deliverer' => '等配送员送货上门哦~',
 	'please_fetch_your_package' => '坐等您来自提点取货哦~',
 	'the_station_is_located_in' => '配送站地址：',
+
+	'weixin_menu_type_view' => '跳转URL',
+	'weixin_menu_type_click' => '点击事件',
+	'weixin_menu_type_scancode_push' => '扫码推事件',
+	'weixin_menu_type_scancode_waitmsg' => '扫码等待事件',
+	'weixin_menu_type_pic_sysphoto' => '弹出系统拍照发图',
+	'weixin_menu_type_pic_photo_or_album' => '弹出拍照或者相册发图',
+	'weixin_menu_type_pic_weixin' => '弹出微信相册发图器',
+	'weixin_menu_type_location_select' => '弹出地理位置选择器',
 );
 
 ?>
