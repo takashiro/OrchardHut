b4cfac82273535f1ffdb36675dc8a286283784bd
Takashiro <takashiro@qq.com>
2015-10-19 13:11:13 +0800

    Accept forced login via GET

--- cart.php
+++ cart.php
@@ -265,7 +265,7 @@ switch($action){
 				//若使用线上支付，进入支付宝界面
 				}elseif($order->paymentmethod != Order::PaidWithCash){
 					if(!empty(Order::$PaymentInterface[$order->paymentmethod])){
-						redirect(Order::$PaymentInterface[$order->paymentmethod].'?orderid='.$order->id);
+						redirect(Order::$PaymentInterface[$order->paymentmethod].'?orderid='.$order->id.'&'.User::COOKIE_VAR.'='.urlencode($_COOKIE[User::COOKIE_VAR]));
 					}
 				}
 
