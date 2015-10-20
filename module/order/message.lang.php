c5f4c1a5e734ddb3981651d1c9adef1b31ff0943
Takashiro <takashiro@qq.com>
2015-10-20 12:37:36 +0800

    Split language pack

---
+++ module/order/message.lang.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+	'successfully_made_comment_for_order' => '感谢您的反馈！',
+	'returned_order_has_been_submitted' => '退货申请已成功提交，我们马上处理~',
+);
---
+++ module/promotion/message.lang.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'if_not_logged_in_comma' => '若您未登录，',
+	'click_here_and_login' => '点击这里登录',
+	'and_reply' => '然后回复',
+	'click_and_enter_shop' => '点击进入商城',
+	'invalid_qrcode' => '该二维码无效。',
+	'your_referrer_is' => '您的推荐人是：',
+	'you_registered_earlier_than_the_referrer' => '您注册比这个用户早多啦~ 肯定不是TA推荐的噜',
+);
