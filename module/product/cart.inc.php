1dd9adb9a3473cfb48222779eda3db0673e0cc59
Takashiro <takashiro@qq.com>
2015-10-28 10:54:33 +0800

    不通过GET传递登录信息

--- module/product/cart.inc.php
+++ module/product/cart.inc.php
@@ -265,7 +265,7 @@ switch($action){
 				//若使用线上支付，进入支付宝界面
 				}elseif($order->paymentmethod != Wallet::ViaCash){
 					if(!empty(Wallet::$PaymentInterface[$order->paymentmethod])){
-						redirect('./?mod='.Wallet::$PaymentInterface[$order->paymentmethod].'&orderid='.$order->id.'&'.User::COOKIE_VAR.'='.urlencode($_COOKIE[User::COOKIE_VAR]));
+						redirect('./?mod='.Wallet::$PaymentInterface[$order->paymentmethod].'&orderid='.$order->id);
 					}
 				}
 
