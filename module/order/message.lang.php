1db73283f8a81e57aaf59ae88f5e7028fa2e6c43
Takashiro <takashiro@qq.com>
2015-10-28 10:12:12 +0800

    Add missing weixin notifications

--- module/order/message.lang.php
+++ module/order/message.lang.php
@@ -3,4 +3,12 @@
 return array(
 	'successfully_made_comment_for_order' => '感谢您的反馈！',
 	'returned_order_has_been_submitted' => '退货申请已成功提交，我们马上处理~',
+
+	'your_order_just_arrived_in_delivery_station' => 'Hi~ 您的订单到配送站啦~\(≧▽≦)/~！',
+	'your_order_is_being_delivered' => '您的订单已从配送站出发，等我啊O(∩_∩)O',
+	'deliverer_is' => '配送员：',
+	'please_wait_for_the_deliverer' => '等配送员送货上门哦~',
+	'please_fetch_your_package' => '坐等您来自提点取货哦~',
+	'the_station_is_located_in' => '配送站地址：',
+	'thanks_for_supporting_please_rank' => '感谢您购买，请点这里给我们打分吧~',
 );
