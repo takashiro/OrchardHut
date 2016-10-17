8ea692c82dbac8b5b0b431c7b33db5a5ad7764c2
Takashiro <takashiro@qq.com>
2016-10-17 20:30:34 +0800

    向MySQL 5严格模式低头

--- module/announcement/install.sql
+++ module/announcement/install.sql
@@ -3,10 +3,10 @@ DROP TABLE IF EXISTS `pre_announcement`;
 CREATE TABLE IF NOT EXISTS `pre_announcement` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
-  `content` text NOT NULL,
+  `content` text NULL,
   `time_start` int(11) unsigned NOT NULL,
   `time_end` int(11) unsigned NOT NULL,
-  `displayorder` tinyint(3) NOT NULL,
+  `displayorder` tinyint(3) NOT NULL DEFAULT '0',
   `dateline` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
